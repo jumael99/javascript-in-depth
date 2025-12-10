b7e244e3e6f427ab2de5c23ceac116f9e9eca74a
Jumael Ahmed Khan <zumael@Jumaels-MacBook-Air.local>
2025-12-10 06:12:02 +0600

    finished array-filter

---
+++ array-filter/3.js
@@ -0,0 +1,21 @@
+/*
+    1. Create the following array called "values":
+        [[1,2,3], [0,0,1], [3,6,9], [0,1,2]]
+
+    2. Create an new array called "hasTwos" that:
+        - filters over "values" and keeps only the sub-arrays
+          that have a 2 in them
+
+    3. Print out both arrays
+
+    "hasTwos" should equal to: [[1,2,3], [0,1,2]]
+
+    HINT: Google for "javascript array includes"
+*/
+
+let values = [[1,2,3], [0,0,1], [3,6,9], [0,1,2]];
+const hasTwos = values.filter((element) => {
+    return element.includes(2);
+})
+console.log(values);
+console.log(hasTwos);
