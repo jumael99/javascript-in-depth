70028e5383cea59dd69df58a0e3fef86568d574a
Jumael Ahmed Khan <zumael@Jumaels-MacBook-Air.local>
2025-12-25 12:57:30 +0600

    fetch-api folder added

---
+++ fetch-api/firstex.js
@@ -0,0 +1,30 @@
+/*
+    1. Install node-fetch if your Node version is less than 18
+       using NPM, and import it in to your file (ES Module style)
+
+    2. Make a request to: https://www.craigslist.org/about/
+       and print out the resulting HTML (text) String, using Fetch
+    3. Compare what you see in Node to visiting the actual page in
+       your Browser - what matches up?
+*/
+
+import fetch from "node-fetch";
+
+const callFunction = async () => {
+    const response = await fetch("https://www.craigslist.org/about/");
+    const html = await response.text();
+    console.log(response);
+}
+
+callFunction();
+
+// import fetch from "node-fetch";
+//
+// fetch("https://www.craigslist.org/about/")
+//     .then((response) => {
+//         return response.text();
+//     })
+//     .then((html) => {
+//         console.log(html);
+//     });
+
